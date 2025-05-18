0ae235e49bedb0cfbbd2048566b6c93a19c0676b
Alice <ms04773@surrey.ac.uk>
2025-05-19 00:15:31 +0100

    update config, app.py and requirements for deployment. add Procfile for deployment

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(host=app.config["HOST"], port=app.config["PORT"], debug=True)
